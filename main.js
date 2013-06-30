71a7df311583d87bf59b80d12550c3eb1fa6f842
David Su <david.d.su@gmail.com>
2013-06-29 20:07:19 -0400

    post-hackathon bonchon
    peachy!

---
+++ main.js
@@ -0,0 +1,201 @@
+﻿
+
+/**CONTEXT**/
+var ctx;
+
+try {
+	ctx = new webkitAudioContext();
+}
+catch (e) {
+	try {
+		ctx = new AudioContext();
+	}
+	catch (e) {
+		alert("Sorry mate, no Web Audio API in your browser. This relationship isn't going to work.");
+	}
+}
+
+/**TUNER**/
+var userPitches = [];
+var tuner = new Tuner(userPitches);
+//var canvas = document.getElementById('candide');
+
+
+/**ENGRAVING**/
+//realtime update is done by Tuner()
+const NOTEMAX=20;
+//stave
+var cv = $('.engraving canvas')[0];
+var renderer = new Vex.Flow.Renderer(cv, Vex.Flow.Renderer.Backends.CANVAS);
+var g_ctx = renderer.getContext();
+var stave = new Vex.Flow.Stave(10, 0, 800);
+stave.addClef("treble").setContext(g_ctx).draw();
+
+//notes
+var notes = [];
+/*
+var notes = [new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" })];
+notes.push(new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }));
+*/
+
+//voice and engrave
+/*
+var voice = new Vex.Flow.Voice({
+num_beats: 4,
+beat_value: 4,
+resolution: Vex.Flow.RESOLUTION
+});
+voice.setStrict(false);
+
+
+//updating voice
+voice.addTickables(notes);
+
+var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 500);
+
+voice.draw(g_ctx, stave);
+
+//voice.addTickable(new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }));
+*/
+
+
+
+//engrave
+var DURATION = "q";
+
+function engraveNew(k, c, p) {
+	//add new note
+	//var new_note = new Vex.Flow.StaveNote();
+	var new_note;
+	
+	if (p.charAt(2) == "") {
+		new_note = new Vex.Flow.StaveNote({ keys: [k], duration: "q" });
+	}
+	else {
+		new_note = new Vex.Flow.StaveNote({ keys: [k], duration: "q" }).addAccidental(0, new Vex.Flow.Accidental("#"));
+	}
+	/*
+	new_note.duration = "q";
+	new_note.keys = [];
+	if (p.charAt(2) != "")
+		new_note.addAccidental(1, new Vex.Flow.Accidental("#"));
+	new_note.keys.push(k);
+	*/
+	
+	//reset staff (justifying won't work and it'll disappear otherwise)
+	if (notes.length > NOTEMAX) {
+		var n = notes[NOTEMAX];
+		notes = [n];
+	}
+	//add notes to array
+	notes.push(new_note);
+	
+	//voice creation
+	var voice = new Vex.Flow.Voice({
+	num_beats: 4,
+	beat_value: 4,
+	resolution: Vex.Flow.RESOLUTION
+	});
+	voice.setStrict(false);
+	
+	//updating voice (& clearing canvas)
+	c.clearRect(0, 0, cv.width, cv.height);
+	stave.setContext(g_ctx).draw(); //bad oop
+	voice.addTickables(notes);
+	var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 800);
+	voice.draw(c, stave);
+}
+
+function clear(pitches, engraves) {
+	pitches = [];
+	engraves = [];
+	console.log("yes, clear!");
+}
+
+/*
+document.getElementById("clearButton").onclick = function() {
+	console.log("CLEAR!");
+	//clear(userPitches, notes);
+	userPitches = [];
+	notes = [];
+	g_ctx.clearRect(0, 0, cv.width, cv.height);
+	stave.setContext(g_ctx).draw(); //bad oop
+};
+*/
+
+/**PEACHNOTE ANALYSIS**/
+var app_id = "ceb08b52";
+var app_key = "22b75b29fd311f4d57105a192870eb7e";
+//var url = "http://www.peachnote.com/rest/api/v0/scoreSearch?site=loc.gov&type=chordAffine&q=62+0+1+2+0+-2+-1&app_id=abe33d61&app_key=b985ac7a9881a67de8a7b069d295b3fe";
+var base_url = "http://www.peachnote.com/rest/api/v0/scoreSearch?site=imslp.org&type=singleNoteAffine";
+var peach_pinch = [];
+
+var out_str = "";
+
+//formatting url for json request
+function submit(original_coll) {
+	var NGRAM_LENGTH = 5;
+	var req = base_url;
+	var coll = absoluteToRelative(original_coll);
+	var melody_str = "";
+	
+	//this takes the ENTIRE melody
+	/*
+	for (var i=0; i<coll.length; i++) {
+		melody_str += coll[i].toString() + "+";
+	}
+	*/
+	//...whereas this only takes NGRAM_LENGTH consecutive notes, spot randomly determined
+	if (coll.length > NGRAM_LENGTH) {
+		var r = Math.floor(Math.random()*(coll.length-NGRAM_LENGTH));
+		for (var i=r; i<NGRAM_LENGTH; i++) {
+			melody_str += coll[i].toString() + "+";
+		}
+	}
+	else {
+		for (var i=0; i<coll.length; i++) {
+		melody_str += coll[i].toString() + "+";
+		}
+	}
+	
+	melody_str = melody_str.substring(0, melody_str.length-1);
+	//console.log(melody_str);
+	
+	req += "&q=" + melody_str;
+	req += "&app_id=" + app_id;
+	req += "&app_key=" + app_key;
+	console.log(req);
+	$.getJSON(req+"&callback=?", null, function(peachdata) {
+		console.log(peach_pinch);
+		console.log("pea");
+		peach_pinch = peachdata;
+	}).done(function() {
+		$("#result").html(parse(peach_pinch));
+	});
+	
+}
+
+
+//e.g. [62, 63, 60] becomes [62, 1, -2]
+function absoluteToRelative(coll) {
+	var newColl = [];
+	var base = coll[0];
+	newColl.push(base);
+	for (var i=1; i<coll.length; i++) {
+		newColl.push(base-coll[i]);
+	}
+	return newColl;
+}
+
+document.getElementById("submitButton").onclick = function() {
+	submit(userPitches);
+};
+
+//peachdata is what gets received when you execute this callback
+/*
+$.getJSON(url+"&callback=?", null, function(peachdata) {
+	console.log(peachdata[0]);
+	console.log("pea");
+	peach_pinch = peachdata;
+});
+*/
---
+++ peach_parse.js
@@ -0,0 +1,106 @@
+﻿var compliments = ["Nice playing!", "Good stuff!", "Sweet work!", "Sounds great!", "Marvelous!", "Tell it!", "Mmmmm!"];
+var DATABASE_THRESHOLD = 10;
+var spotifyURI = "spotify:trackset:PlaylistName:";
+var spotifyData;
+
+function parse(database) { //database = what was just played cross-refed with the database
+	var out = "<b>";
+	//var spotifyURI_tracks = "";
+	
+	out += compliments[Math.floor(Math.random()*compliments.length)];
+	out += " Check out these existing musical passages that match the melodic content of what you just played:</b><p>";
+	
+	//displaying aforementioned content
+	var max;
+	var newData = fisherYates(database);
+	
+	if (database.length == 0) {
+		out += "No existing passages found! A regular musical idiosyncrat, you are.<br>";
+	}
+	else if (database.length < DATABASE_THRESHOLD) {
+		max = database.length;
+	}
+	else {
+		max = DATABASE_THRESHOLD;
+	}
+	
+	for (var i=0; i<max; i++) {
+		
+		//displaying data in out stream
+		out += "<i>" + newData[i]["title"] + "</i> by " + newData[i]["composer"] + " (" + newData[i]["year"] + "), " + "<a href='" + pageLink(newData[i]) + "' target='_blank'>page(s) " + newData[i]["pages"] + "</a><br>";
+		
+		//generating spotify playlist
+		var trackID;
+		//search url construction
+		var searchURL = "http://ws.spotify.com/search/1/track.json?q=" + newData[i]["title"] + " " + newData[i]["composer"] + "&page=1";
+		searchURL = searchURL.replace(/ /g, "%20");
+		searchURL = searchURL.replace(/\,/g, "");
+		
+		console.log(searchURL);
+		
+		if (i == max-1) {
+			$.getJSON(searchURL, null, function(data) {
+				//console.log(data);
+				spotifyData = data;
+			}).done(function() {
+				trackID = spotifyData["tracks"][Math.floor(Math.random()*spotifyData["tracks"].length)]["href"];
+				trackID = trackID.replace("spotify:track:", "");
+				console.log(trackID);
+				spotifyURI += trackID + ",";
+				console.log(spotifyURI);
+				
+				out += "<p><b>Below is a Spotify playlist based on your improvisational vocabulary; you might find some further inspiration, direction, and stimulation in these recordings. Happy listening, and keep making music!</b><p>";
+	
+				console.log(i + " URI: " + spotifyURI);
+				out += '<iframe src="https://embed.spotify.com/?uri=' + spotifyURI + '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>';
+				
+				$("#result").html(out);
+			});
+		}
+		
+		else {
+			$.getJSON(searchURL, null, function(data) {
+				//console.log(data);
+				spotifyData = data;
+			}).done(function() {
+				trackID = spotifyData["tracks"][Math.floor(Math.random()*spotifyData["tracks"].length)]["href"];
+				trackID = trackID.replace("spotify:track:", "");
+				console.log(trackID);
+				spotifyURI += trackID + ",";
+				console.log(i + ": " + spotifyURI);
+				
+			});
+		}
+
+	}
+
+	//displaying spotify playlist
+	//format: <iframe src="https://embed.spotify.com/?uri=spotify:trackset:PlaylistName:3Z7u3vmPG8pJhynCvHlPt5,0579b3RdMpVdwMHWsyrwTE" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>	
+	
+	
+	
+	return out;
+}
+
+//Fisher-Yates shuffle algorithm (adapted from http://sedition.com/perl/javascript-fy.html)
+function fisherYates ( oldArray ) {
+  var myArray = oldArray;
+  var i = myArray.length;
+  if ( i == 0 ) return false;
+  while ( --i ) {
+     var j = Math.floor( Math.random() * ( i + 1 ) );
+     var tempi = myArray[i];
+     var tempj = myArray[j];
+     myArray[i] = tempj;
+     myArray[j] = tempi;
+   }
+  return myArray;
+}
+
+//generates a link to peachnote score viewer for specific page
+function pageLink(piece) {
+	var id = piece["id"];
+	var pages = piece["pages"];
+	var url = "http://www.peachnote.com/viewer?sid="+id+"&page="+pages; //does "&page=64" give different result from "&page=64,65"?
+	return url;
+}
